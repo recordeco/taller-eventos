71d0d7851a2b26073d029b8f9b61871cc045aff0
recordeco <contacto.ivanpereira@gmail.com>
2023-08-05 01:06:35 -0300

    Ultimo paso

---
+++ eventos.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const divConBoton = document.getElementById('divConBoton');
+    
+    divConBoton.addEventListener('click', function() {
+      alert('Hola! Soy el div');
+    });
+  });

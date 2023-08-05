3ea95b6265921be6b7bfe1464394cf8d44347438
recordeco <contacto.ivanpereira@gmail.com>
2023-08-05 01:30:39 -0300

    Solucion Click

--- eventos.js
+++ eventos.js
@@ -1,7 +1,12 @@
-
 document.addEventListener('DOMContentLoaded', function() {
-    const divConBoton = document.getElementById('divConBoton');
-    
+    const boton = document.getElementById('miBoton');
+    const divConBoton = document.getElementById('miDiv');
+  
+    boton.addEventListener('click', function(event) {
+      event.stopPropagation(); // Detenemos la propagación del evento click del botón
+      alert('Has hecho clic en el botón');
+    });
+  
     divConBoton.addEventListener('click', function() {
       alert('Hola! Soy el div');
     });
